ead817fadf3454ee4caf57b4b395b6bd09c32262
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-07-23 09:03:17 +0530

    solved uva230

--- uva230.cpp
+++ uva230.cpp
@@ -1,18 +1,58 @@
-#include <iostream>
-#include <set>
-#include <string>
-#include <map>
-#include <fstream>
-#include <iomanip>
+/*
+ * Siddhartha Tiwari
+ * Date: 22-08-2020
+ */
 
+#include <bits/stdc++.h>
 using namespace std;
-
-void print_pair(set<pair<string, string>> som, ostream& fout) {
-	for(auto it : som)
-		fout << it.first << " " << it.second << "\n";
-	fout << "\n";
-	return;
-}
+// #define MYCOMP 1
+#ifdef MYCOMP
+class pscr {
+   public:
+    pscr() { cerr << "\n"; }
+    template <class T,
+              typename enable_if<is_fundamental<T>::value, T>::type* = nullptr>
+    pscr operator<<(T var) {
+        cerr << var;
+        return *this;
+    }
+    template <class T, typename enable_if<!is_fundamental<T>::value &&
+                                              !is_pointer<T>::value,
+                                          T>::type* = nullptr>
+    pscr operator<<(T& var) {
+		typedef typename T::value_type temp_type;
+		vector<temp_type> temp;
+		temp.insert(temp.begin(), var.begin(), var.end());
+		auto it = temp.begin();
+        for (; it != (--temp.end()); it++)
+            *this << *it << string(", ");
+		*this << *it;
+        return *this;
+    }
+    template <class T,
+              typename enable_if<is_pointer<T>::value, T>::type* = nullptr>
+    pscr operator<<(T var) {
+        cerr << "Can't print an array. Try something different.\n";
+        return *this;
+    }
+    template <class T1, class T2>
+    pscr operator<<(pair<T1, T2> var) {
+        *this << "(" << var.first << ", " << var.second
+              << ")";
+        return *this;
+    }
+    pscr operator<<(const char str[]) {
+		string tstr(str);
+		cerr << tstr;
+		return *this;
+	}
+    pscr operator<<(string var) {
+        cerr << var;
+        return *this;
+    }
+};
+#endif
+#define debug(...) "[[" << #__VA_ARGS__ << ": " << __VA_ARGS__ << "]]\t\t"
 
 int main() {
 	fstream fin("inp.txt", fstream::in);
@@ -21,23 +61,19 @@ int main() {
 	set<pair<string, string>> titles, borrowed, returned;
 	map<string, string> ass;
 	while(true) {
-		fin.getline(tstr, 100000);
+		cin.getline(tstr, 100000);
 		string str(tstr);
 		if(str == "END")
 			break;
 		str.erase(0, 1);
-		cout << str << "\n";
 		size_t fnd = str.find("\"");
 		string s1 = str.substr(0, fnd);
 		string s2 = str.substr(fnd+5);
 		ass[s1] = s2;
 		titles.insert(make_pair(s2, s1));
-		print_pair(titles, fout);
 	}
-	fout << "Here\n";
-	fout.close();
 	while(true) {
-		fin.getline(tstr, 10000);
+		cin.getline(tstr, 10000);
 		string str(tstr);
 		if(str == "END")
 			break;
@@ -47,21 +83,23 @@ int main() {
 			scnd.erase(0, 1);
 			scnd.erase(scnd.size()-1, 1);
 			if(frst == "BORROW") {
-				titles.erase(titles.find(make_pair(scnd, ass[scnd])));
+				titles.erase(titles.find(make_pair(ass[scnd], scnd)));
 				borrowed.insert(make_pair(scnd, ass[scnd]));
 			}
 			else
-				returned.insert(make_pair(scnd, ass[scnd]));
+				returned.insert(make_pair(ass[scnd], scnd));
 		}
 		else {
 			for(auto it : returned) {
 				titles.insert(it);
 				auto tt = titles.find(it);
 				if(tt == titles.begin())
-					fout << "Put \"" << it.first << "\" first\n";
+					cout << "Put \"" << it.second << "\" first\n";
 				else
-					fout << "Put \"" << it.first << "\" after \"" << (*(--tt)).first << "\"\n";
+					cout << "Put \"" << it.second << "\" after \"" << (*(--tt)).second << "\"\n";
 			}
+			cout << "END\n";
+			returned.clear();
 		}
 	}
 	return 0;
