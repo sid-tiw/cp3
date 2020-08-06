d83dfb96a98abee429ff704a2aae130c557d5f37
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-08-06 23:51:52 +0530

    added the matrix class

--- binary_exponentiation.cpp
+++ binary_exponentiation.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-template <class T>
-T binary_exp_rec(T a, T b) {
+template <class T, class POW>
+T binary_exp_rec(T a, POW b) {
     T ans, ml = 1;
-	if (!b) return 1;
+    if (!b) return 1;
     if (b % 2 == 0) ans = binary_exp_rec(a, b / 2);
     if (b % 2 != 0) {
         ans = binary_exp_rec(a, (b - 1) / 2);
@@ -15,18 +16,33 @@ T binary_exp_rec(T a, T b) {
     return ans * ans * ml;
 }
 
-template<class T>
-T binary_exp_iter(T a, T b) {
-	T res = 1;
-	while (b > 0) {
-		if (b & 1) res *= a;
-		a *= a;
-		b >>= 1;
-	}
-	return res;
+template <class T, class POW>
+T binary_exp_iter(T a, POW b) {
+    T res = 1;
+    while (b > 0) {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
 }
 
+template <class T>
+class Matrix {
+    vector<vector<T>> obj;
+
+   public:
+    Matrix(int rows, int cols) { obj = vector<vector<T>>(rows, vector<T>(cols)); }
+    Matrix(int rows, int cols, T val) {
+        obj = vector<vector<T>>(rows, vector<T>(cols, val));
+    }
+    vector<T> operator[](int val) {
+        return obj[val];
+    }
+};
+
 int main() {
-	cout << binary_exp_iter(3, 5);
-	return 0;
+	Matrix<int> A(4, 5, -1);
+    cout << binary_exp_iter(3, 5);
+    return 0;
 }
